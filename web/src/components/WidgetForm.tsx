367681d6d6a27102e2f2ea24757197db6718a7cd
Carlos Eduardo <dudu_carlos.eduardo@hotmail.com>
2022-05-08 17:18:30 -0300

    added form images

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,5 +1,33 @@
 import { CloseButton } from "./CloseButton";
 
+import bugImageUrl from "../assets/bug.svg"
+import ideaImageUrl from "../assets/idea.svg"
+import thoughtImageUrl from "../assets/thought.svg"
+
+const FeedbackTypes = {
+    BUG: {
+        title:'Problema',
+        image:{
+            source: bugImageUrl,
+            alt: 'Imagem de um inseto'
+        },
+    },
+    IDEA: {
+        title:'Ideia',
+        image:{
+            source: ideaImageUrl,
+            alt: 'Imagem de uma lampada'
+        },
+    },
+    OTHER: {
+        title:'Outro',
+        image:{
+            source: thoughtImageUrl,
+            alt: 'Imagem de um balão de pensamento'
+        },
+    },
+}
+
 export function WidgetForm() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
